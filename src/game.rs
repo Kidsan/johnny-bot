c9771f485a08d0a233c52717dcf6d454f4491e5c
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-04-06 18:50:46 +0200

    test: test the odds for coin flips

--- src/game.rs
+++ src/game.rs
@@ -84,8 +84,69 @@ impl CoinGame {
     pub fn get_winner(&self, rng: &mut rand::rngs::StdRng) -> String {
         let num = rng.gen_range(0..100);
         if num < 1 {
-            return "side".to_owned();
+            "side".to_owned()
+        } else if num < 50 {
+            "heads".to_owned()
+        } else {
+            "tails".to_owned()
         }
-        ["heads", "tails"].choose(rng).unwrap().to_string()
+    }
+}
+
+// test
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_coin_game_get_winner() {
+        let mut rng = rand::SeedableRng::seed_from_u64(42);
+        let game = CoinGame {
+            id: "1".to_owned(),
+            players: vec!["player1".to_owned(), "player2".to_owned()],
+            heads: vec!["player1".to_owned()],
+            tails: vec!["player2".to_owned()],
+            amount: 100,
+            pot: 200,
+            deadline: time::Instant::now(),
+        };
+        let winner = game.get_winner(&mut rng);
+        assert!(winner == "heads" || winner == "tails" || winner == "side");
+    }
+
+    #[test]
+    fn test_coin_game_get_winner_odds() {
+        let mut rng = rand::SeedableRng::from_entropy();
+        let game = CoinGame {
+            id: "1".to_owned(),
+            players: vec!["player1".to_owned(), "player2".to_owned()],
+            heads: vec!["player1".to_owned()],
+            tails: vec!["player2".to_owned()],
+            amount: 100,
+            pot: 200,
+            deadline: time::Instant::now(),
+        };
+
+        let mut heads = 0;
+        let mut tails = 0;
+        let mut side = 0;
+
+        let num_games = 1000000;
+
+        for _i in 0..num_games {
+            let a = game.get_winner(&mut rng);
+            if a == "heads" {
+                heads += 1;
+            } else if a == "tails" {
+                tails += 1;
+            } else {
+                side += 1;
+            }
+        }
+
+        assert!(num_games as f64 * 0.03 > side as f64);
+
+        // test that the ratio of heads to tails is close to 1
+        assert!((heads as f64 / tails as f64) - 1.0 < 0.01);
     }
 }
