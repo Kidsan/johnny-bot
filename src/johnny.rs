c5fd073ec3c2fb37da2fd6ecce8f1e17aa8ac205
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-08-03 14:52:10 +0200

    johnny: improve timers

--- src/johnny.rs
+++ src/johnny.rs
@@ -44,7 +44,9 @@ impl Johnny {
         }
     }
     pub async fn start(&self, signal: std::sync::mpsc::Receiver<()>) {
-        let mut time_passed = 0;
+        let mut minute_counter = tokio::time::Instant::now();
+        let mut five_minute_counter = tokio::time::Instant::now();
+        let mut last_lottery: Option<chrono::NaiveDateTime> = None;
         loop {
             match signal.try_recv() {
                 Ok(_) => {
@@ -57,20 +59,23 @@ impl Johnny {
                     break;
                 }
             }
-
-            if time_passed % 60 == 0 {
-                let _ = rand::thread_rng().gen_range(0..=100);
-                self.check_should_trigger_lottery().await;
-                self.refresh_config().await;
+            if self.should_trigger_lottery(last_lottery).await {
+                last_lottery = Some(chrono::Utc::now().naive_utc());
+                self.lottery().await;
             }
 
-            if time_passed % 300 == 0 {
+            if minute_counter.elapsed().as_secs() >= 60 {
+                let _ = rand::thread_rng().gen_range(0..=100);
+                self.refresh_config().await;
+                minute_counter = tokio::time::Instant::now();
+            }
+
+            if five_minute_counter.elapsed().as_secs() >= 300 {
                 self.decay().await;
                 self.check_skewed_odds().await;
-                time_passed = 0;
+                five_minute_counter = tokio::time::Instant::now();
             }
             tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-            time_passed += 5;
         }
     }
 
@@ -154,16 +159,24 @@ impl Johnny {
         self.config.write().unwrap().bot_odds = bot_odds;
     }
 
-    pub async fn check_should_trigger_lottery(&self) {
+    pub async fn should_trigger_lottery(&self, last: Option<chrono::NaiveDateTime>) -> bool {
         let time = chrono::Utc::now()
             .time()
             .with_second(0)
             .unwrap()
             .with_nanosecond(0)
             .unwrap();
-        if time == NaiveTime::from_hms_opt(18, 00, 0).unwrap() || self.dev_env {
-            self.lottery().await;
+
+        let today = chrono::Utc::now().date_naive();
+        let time = today.and_time(time).time();
+
+        if (time == NaiveTime::from_hms_opt(18, 00, 0).unwrap()
+            && (last.is_none() || last.unwrap().date() != today))
+            || self.dev_env
+        {
+            return true;
         }
+        false
     }
 
     pub async fn lottery(&self) {
