7ca559ae130c52ecbc430bdd4dfa329a43e7f8fa
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-08-31 11:35:54 +0200

    refactor: consolidate emoji usage

--- src/discord.rs
+++ src/discord.rs
@@ -1,2 +1,7 @@
 pub const NICKNAME_LICENCE: u64 = 1227572071874297876;
 pub const EGG_ROLE: u64 = 1275504308829425806;
+pub const JBUCK_EMOJI: &str = "<:jbuck:1228663982462865450>";
+pub const DOGE_TROLL_EMOJI_1: &str = "<:dogeTroll:1160530414490886264>";
+pub const DOGE_TROLL_EMOJI_2: &str = "<:doge:1160530341681954896>";
+pub const DOGE_PRAY_EMOJI: &str = "<:dogePray1:1186283357210947584>";
+pub const DOGE_CRY_EMOJI: &str = "<:dogeCrying:1160530365413330974>";
